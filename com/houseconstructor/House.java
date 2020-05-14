79ee248fa81cb289abe8d3b7255df2a4dd93865c
burrell <eewinterz@gmail.com>
2020-05-13 21:48:05 -0400

    update

--- com/houseconstructor/House.java
+++ com/houseconstructor/House.java
@@ -5,7 +5,16 @@ public class House {
 	private Bedroom bedroom;
 	private Bathroom bathroom;
 	
-	public House() {
-		
+	public House(Bedroom bedroom, Bathroom bathroom) {
+		this.bathroom = bathroom;
+		this.bedroom = bedroom;
+	}
+
+	public Bedroom getBedroom() {
+		return bedroom;
+	}
+
+	public Bathroom getBathroom() {
+		return bathroom;
 	}
 }
--- com/houseconstructor/Main.java
+++ com/houseconstructor/Main.java
@@ -3,6 +3,10 @@ package com.houseconstructor;
 public class Main {
 
 	public static void main(String[] args) {
+		Bathroom bath = new Bathroom();
+		Bedroom bed = new Bedroom();
+		House home = new House(bed, bath);
 		
+		home.getBathroom().getDescription();
 	}
 }
