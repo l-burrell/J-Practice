3fd4eb75d8e04a435712f04580a2f05c0d896087
burrell <eewinterz@gmail.com>
2020-05-14 13:15:13 -0400

    update

---
+++ pop/one/Arrays.java
@@ -0,0 +1,55 @@
+package pop.one;
+
+import java.util.Scanner;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		System.out.println("\nArray 1: ");
+		int[] num = new int[5];
+		num[0] = 23;
+		num[1] = 4543;
+		num[2] = 392;
+		num[4] = 544;
+		printArray(num);
+		System.out.println("The sum of this array is: " + sumArrayValues(num));
+		
+		System.out.println("\nArray 2: ");
+		char[] anotherArr = {'A', 'c', 'h'};
+		printArray(anotherArr);
+		
+		printArray(getInput(4));
+		
+		
+	}
+	
+	public static void printArray(int[] array) {
+		for(int i = 0; i < array.length; i++) {
+			System.out.println("position " + i + " is int value: " + array[i]);
+		}
+	}
+	
+	public static void printArray(char[] array) {
+		for(int i = 0; i < array.length; i++) {
+			System.out.println("position " + i + " is char value: " + array[i]);
+		}
+	}
+	
+	public static int[] getInput(int num) {
+		System.out.println("\nPlease insert " + num + " integer values: ");
+		Scanner scan = new Scanner(System.in);
+		int[] array = new int[num];
+		for(int i = 0; i < array.length; i++) {
+			array[i] = scan.nextInt();
+		}
+		return array;
+	}
+	
+	public static int sumArrayValues(int[] array) {
+		int sum = 0;
+		for(int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+		return sum;
+	}
+}
